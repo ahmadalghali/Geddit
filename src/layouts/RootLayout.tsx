e0bdc21580dd6b86216940c025c52cbb11c33082
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-08-13 20:18:34 +0100

    fix horizontal padding on big screen sizes

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -5,7 +5,7 @@ function RootLayout() {
   return (
     <main>
       <Header />
-      <section className='max-w-3xl mx-auto pt-20 pb-8 md:px-10 px-5 min-h-screen flex flex-col'>
+      <section className='max-w-3xl mx-auto pt-20 pb-8 sm:px-10 px-5 min-h-screen flex flex-col'>
         <Outlet />
       </section>
     </main>

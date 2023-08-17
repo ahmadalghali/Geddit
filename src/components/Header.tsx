352219f14b97b63a6007f6bae554700c521d155a
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-08-17 18:49:32 +0100

    fix: make navbar content responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,25 +5,27 @@ import { Link, useNavigate } from "react-router-dom";
 function Header() {
   const navigate = useNavigate();
   return (
-    <div className='bg-amber-500 flex items-center justify-between px-5 py-3 fixed inset-x-0 z-50 shadow-md'>
-      <div onClick={() => navigate("/")} className='cursor-pointer'>
-        <Logo />
+    <div className='bg-amber-500 fixed inset-x-0 z-50 shadow-md'>
+      <div className='flex items-center py-3 sm:px-10 px-5 justify-between space-x-5 mx-auto  max-w-3xl'>
+        <div onClick={() => navigate("/")} className='cursor-pointer '>
+          <Logo />
+        </div>
+        <Link to={`/explore`}>
+          <IconWorld size={30} className='cursor-pointer text-amber-900' />
+        </Link>
+
+        <SearchBar />
+        <Link to={`/create-community`}>
+          <IconPlus size={30} className='cursor-pointer text-amber-900' />
+        </Link>
+        <Link to={`/create-post`}>
+          <IconPencil size={30} className='cursor-pointer text-amber-900' />
+        </Link>
+
+        <Link to={`/register`}>
+          <IconUser className='cursor-pointer text-amber-900' size={30} />
+        </Link>
       </div>
-      <Link to={`/explore`}>
-        <IconWorld size={30} className='cursor-pointer text-amber-900' />
-      </Link>
-
-      <SearchBar />
-      <Link to={`/create-community`}>
-        <IconPlus size={30} className='cursor-pointer text-amber-900' />
-      </Link>
-      <Link to={`/create-post`}>
-        <IconPencil size={30} className='cursor-pointer text-amber-900' />
-      </Link>
-
-      <Link to={`/register`}>
-        <IconUser className='cursor-pointer text-amber-900' size={30} />
-      </Link>
     </div>
   );
 }

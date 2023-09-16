4bc4c21873d7466918afe80945d00d6b9151927e
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-09-16 14:35:24 +0100

    redirect to community on click

--- src/components/SearchResultCommunity.tsx
+++ src/components/SearchResultCommunity.tsx
@@ -3,6 +3,7 @@ import { IconBrandReddit } from "@tabler/icons-react";
 import { Constants } from "@/lib/constants";
 import { CommunitySummaryDTO } from "@/types/dtos";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 type Props = {
   community: CommunitySummaryDTO;
 };
@@ -14,29 +15,31 @@ function SearchResultCommunity({ community }: Props) {
     alert("joined");
   };
   return (
-    <motion.div className='flex rounded-md p-3 items-center '>
-      <Avatar size='lg' radius='xl' color='cyan'>
-        <IconBrandReddit size='35' />
-      </Avatar>
+    <Link to={`/${Constants.PREFIX_COMMUNITY}${community.name}`}>
+      <motion.div className='flex rounded-md p-3 items-center '>
+        <Avatar size='lg' radius='xl' color='cyan'>
+          <IconBrandReddit size='35' />
+        </Avatar>
 
-      <div className='ml-3'>
-        <h4 className='font-bold'>
-          {Constants.PREFIX_COMMUNITY}
-          {community.name}
-        </h4>
-        <p className='text-xs text-gray-500'>{community.description}</p>
-      </div>
-      <Button
-        className='ml-auto'
-        sx={{
-          fontWeight: "800",
-        }}
-        radius={"xl"}
-        onClick={handleJoinClicked}
-      >
-        JOIN
-      </Button>
-    </motion.div>
+        <div className='ml-3 mr-1'>
+          <h4 className='font-bold'>
+            {Constants.PREFIX_COMMUNITY}
+            {community.name}
+          </h4>
+          <p className='text-xs text-gray-500'>{community.description}</p>
+        </div>
+        <Button
+          className='ml-auto'
+          sx={{
+            fontWeight: "800",
+          }}
+          radius={"xl"}
+          onClick={handleJoinClicked}
+        >
+          JOIN
+        </Button>
+      </motion.div>
+    </Link>
   );
 }
 

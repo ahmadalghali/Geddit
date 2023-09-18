328ea61f0604ebf54b1bef4f5058b7766174fe74
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-09-18 14:27:48 +0100

    WIP: improve create community form

--- src/pages/CreateCommunityPage.tsx
+++ src/pages/CreateCommunityPage.tsx
@@ -1,40 +1,86 @@
-import { Button, TextInput } from "@mantine/core";
+import { Button, TextInput, Textarea } from "@mantine/core";
 import { useState } from "react";
 import { createCommunity } from "@/api/communities";
 import { CreateCommunityDTO } from "@/types/dtos";
 import PageTitle from "@/components/PageTitle";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { notifications } from "@mantine/notifications";
+import { IconCheck } from "@tabler/icons-react";
+import { Constants } from "@/lib/constants";
+import { useNavigate } from "react-router-dom";
+
+type Inputs = {
+  name: string;
+  description: string;
+};
 
 function CreateCommunityPage() {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
+  const navigate = useNavigate();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<Inputs>();
 
-    if (name.trim().length == 0 || description.trim().length == 0) return;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ name, description }) => {
+    setIsSubmitting(true);
 
     const createCommunityDTO: CreateCommunityDTO = {
-      name: name.trim(),
-      description: description.trim(),
+      name,
+      description,
     };
 
-    createCommunity(createCommunityDTO);
-  };
+    try {
+      const createdCommunity = await createCommunity(createCommunityDTO);
+      // await addArtificialDelay(1);
 
+      if (createdCommunity) {
+        notifications.show({
+          color: "green",
+          title: "Your community has been created!",
+          message: "",
+          icon: <IconCheck size='1rem' />,
+          autoClose: 4000,
+        });
+
+        navigate(`/${Constants.PREFIX_COMMUNITY}${createdCommunity.name}`);
+      } else {
+        notifications.show({
+          message: "Something went wrong, please try again.",
+          color: "red",
+        });
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
     <>
       <PageTitle>Create a community</PageTitle>
-      <form className='shadow-md p-3 rounded-md' onSubmit={(e) => handleSubmit(e)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <TextInput
-          placeholder='Name'
           className='mb-5'
-          required
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          placeholder='Name'
+          icon={<p className='text-[.95rem] text-gray-500 font-bold tracking-widest w-2'>g/</p>}
+          iconWidth={25}
+          {...register("name", { required: true, maxLength: 20 })}
         />
-        <TextInput placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)} />
-        <Button type='submit' className='mt-5'>
-          Submit
+        <Textarea
+          placeholder='Description'
+          minRows={3}
+          {...register("description", { maxLength: 100 })}
+          maxLength={100}
+        />
+        <Button
+          type='submit'
+          className='mt-5 float-right transition-all duration-300'
+          radius={"xl"}
+          disabled={!isValid}
+        >
+          Create community
         </Button>
       </form>
     </>

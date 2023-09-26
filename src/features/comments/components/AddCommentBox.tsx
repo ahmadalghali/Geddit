9b5580e22f08c2e6e2c80680af99e125a18d8105
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-09-26 06:34:49 +0100

    styling changes to add comment box

--- src/features/comments/components/AddCommentBox.tsx
+++ src/features/comments/components/AddCommentBox.tsx
@@ -28,7 +28,7 @@ function AddCommentBox({
   };
 
   return (
-    <form onSubmit={handleSubmit} className={cn("border-2 rounded-md", className)}>
+    <form onSubmit={handleSubmit} className={cn("border-2 border-gray-200 rounded-md", className)}>
       <Textarea
         placeholder='Add a comment'
         minRows={5}
@@ -38,7 +38,17 @@ function AddCommentBox({
         styles={{ input: { border: "none" } }}
       />
       <div className='flex justify-end bg-gray-100 p-2'>
-        <Button type='submit' radius={"xl"} className='ml-auto transition-all duration-300' disabled={!submittable}>
+        <Button
+          type='submit'
+          radius={"xl"}
+          sx={{
+            fontWeight: "bold",
+            color: "white",
+            ":disabled": { background: "darkgray", color: "rgb(239, 239, 239)" },
+          }}
+          className='ml-auto transition-all duration-300'
+          disabled={!submittable}
+        >
           Reply
         </Button>
       </div>

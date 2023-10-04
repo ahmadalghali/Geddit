69c22ca24937e0aea9beac38f3e89ea255b31c5b
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-10-04 07:04:16 +0300

    WIP: refactor sign in page

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,15 +1,28 @@
 import { signIn } from "@/api/auth";
+import { useAuthContext } from "@/contexts/AuthContext";
 import { UserSignInRequestDTO } from "@/types/dtos";
-import { Button, PasswordInput, TextInput } from "@mantine/core";
+import { Avatar, Button, PasswordInput, TextInput } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import { IconBrandReddit } from "@tabler/icons-react";
 
 import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+
+type Inputs = {
+  username: string;
+  password: string;
+};
 
 function SignInPage() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<Inputs>();
+  const { storeDetails } = useAuthContext();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ username, password }) => {
+    setIsSubmitting(true);
 
     const usernameTrimmed = username.trim();
     const passwordTrimmed = password.trim();
@@ -21,28 +34,54 @@ function SignInPage() {
       password: passwordTrimmed,
     };
 
-    signIn(userSignInRequestDTO);
+    try {
+      const user = await signIn(userSignInRequestDTO);
+      if (user) {
+        storeDetails(user);
+        notifications.show({
+          color: "green",
+          title: `Welcome, ${user.username}`,
+          message: "",
+        });
+
+        navigate("/");
+      } else {
+        notifications.show({
+          message: "Something went wrong, please try again.",
+          color: "red",
+        });
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
   };
+
   return (
     <>
-      <h1>Sign In</h1>
-      <form className='shadow-md p-3 rounded-md' onSubmit={(e) => handleSubmit(e)}>
-        <TextInput
-          className='mb-5'
-          value={username}
-          placeholder='Username'
-          required
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <PasswordInput
-          className='mb-5'
-          value={password}
-          placeholder='Password'
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button type='submit'>Sign in</Button>
-      </form>
+      <div className='grid place-items-center pb-5'>
+        <Avatar size='xl' radius='100%' color='orange'>
+          <IconBrandReddit className='text-center' size={"60"} color='orange' />
+        </Avatar>
+        <p className='font-extrabold text-3xl text-gray-600 mt-5'>Sign in</p>
+        <form onSubmit={handleSubmit(onSubmit)} className='mt-10 w-80'>
+          <TextInput placeholder='Username' type='text' {...register("username", { required: true })} />
+          <PasswordInput className='my-5' placeholder='Password' {...register("password", { required: true })} />
+          <Button
+            type='submit'
+            w={"100%"}
+            radius={"xl"}
+            sx={{
+              fontWeight: "800",
+            }}
+          >
+            Sign in
+          </Button>
+        </form>
+
+        <Link to='/register' className='mt-10 font-bold text-gray-600'>
+          Don't have an account? <span className='underline cursor-pointer hover:'>Register</span>
+        </Link>
+      </div>
     </>
   );
 }

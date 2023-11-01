e1d6996985e3d32f6c20fed67f28f2cd497885be
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-11-01 21:30:44 +0000

    restructure header export

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import SearchBar from "@/features/search/components/SearchBar";
+import NavMenu from "@/components/Header/NavMenu";
+import { IconDoorEnter, IconLogin, IconUser } from "@tabler/icons-react";
+import { useDisclosure } from "@mantine/hooks";
+import SignInModal from "@/features/auth/components/SignInModal";
+import { useAuthContext } from "@/contexts/AuthContext";
+
+function Header() {
+  const displayLoginModal = () => {};
+
+  return (
+    <>
+      {/* <SignInModal /> */}
+      <div className='bg-amber-500 fixed inset-x-0 z-50 shadow-md'>
+        <div className='flex items-center py-3 sm:px-10 px-5 justify-between space-x-5 mx-auto  max-w-3xl'>
+          <Link to='/' className='cursor-pointer'>
+            <Logo />
+          </Link>
+
+          <SearchBar />
+
+          <NavMenu />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Logo() {
+  return <p className='font-black text-2xl text-amber-900'>Geddit</p>;
+}
+export default Header;

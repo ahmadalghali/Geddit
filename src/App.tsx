ab471c0b41b31412a866294ca08ec38509f56008
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-11-04 13:05:53 +0000

    google auth wip

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,27 @@ import router from "./Router";
 import { Notifications } from "@mantine/notifications";
 import { ModalsProvider } from "@mantine/modals";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
 
 const cache = createEmotionCache({
   key: "mantine",
   prepend: false,
 });
 
+const clientId = "870485872584-5sgtcd4i1rcnq5uc0a7mq15arq14u55a.apps.googleusercontent.com";
+
 function App() {
+  useEffect(() => {
+    const start = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    };
+
+    gapi.load("client:auth2", start);
+  }, []);
   return (
     <MantineProvider emotionCache={cache} withGlobalStyles withNormalizeCSS>
       <ModalsProvider>

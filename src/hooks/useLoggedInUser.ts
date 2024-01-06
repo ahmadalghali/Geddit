e23b319c04ec5fd70fcd3d3dd126dfb3ffe48fa4
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2024-01-06 02:10:48 +0000

    remove hardcoded values and fix build

--- src/hooks/useLoggedInUser.ts
+++ src/hooks/useLoggedInUser.ts
@@ -2,7 +2,7 @@ import { UserDTO } from "@/types/dtos";
 import { useState } from "react";
 
 function useLoggedInUser() {
-  const [user, setUser] = useState<UserDTO | null>({ id: "123", username: "ahmad" });
+  const [user, setUser] = useState<UserDTO | null>();
 
   return {
     user,

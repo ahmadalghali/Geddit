905c49200da4b6d7e47e3dfd7ef295387b2d3457
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2024-01-08 09:31:48 +0000

    comment out dayjs temporarily to fix bug in production

--- src/lib/utils/date-time.ts
+++ src/lib/utils/date-time.ts
@@ -1,7 +1,13 @@
-import * as dayjs from "dayjs";
+import * as day from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-dayjs.extend(relativeTime);
+day.extend(relativeTime);
 
-export function since(date?: string | number | dayjs.Dayjs | Date | null | undefined) {
-  return dayjs(date).fromNow();
+// export function since(date?: string | number | day.Dayjs | Date | null | undefined) {
+//   return day(date).fromNow();
+// }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function since(date?: string) {
+  date;
+  return "test";
 }
